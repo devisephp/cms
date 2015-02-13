d370ac26dd3fa59a40360e090aa894eb99cc0a69
Chad Lockett <chad@lbm.co>
2015-02-13 15:56:47 -0500

    Init. of PermissionsRepo test.

--- tests/functional/Users/Permissions/PermissionsRepositoryTest.php
+++ tests/functional/Users/Permissions/PermissionsRepositoryTest.php
@@ -4,9 +4,27 @@ use Mockery as m;
 
 class PermissionsRepositoryTest extends \DeviseTestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->Framework =  new \Devise\Support\Framework;
+        $this->Framework->Config = m::mock('Illuminate\Config\Repository');
+        $this->Framework->Input = m::mock('Illuminate\Http\Request');
+        $this->Framework->View = m::mock('Illuminate\View\Factory');
+        $this->Framework->Paginator = m::mock('Devise\Support\DevisePaginator');
+
+        $this->PermissionsRepository = new PermissionsRepository($this->Framework);
+    }
+
     public function test_it_can_get_all_permissions()
     {
-        $this->markTestIncomplete();
+        // gets a permissions config fixture (of what we expect bacl)
+        $permissionsFixture = $this->fixture('devise-configs.permissions');
+
+        $this->Framework->Config->shouldReceive('get')->once()->andReturn($permissionsFixture);
+
+        assertArrayHasKey('isDeviseAdmin', $this->PermissionsRepository->getAllPermissions());
     }
 
     public function test_it_can_get_permission_by_path()

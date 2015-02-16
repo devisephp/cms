b47ce46c624453311b5b27201c5147faa2ac489a
Chad Lockett <chad@lbm.co>
2015-02-16 14:49:52 -0500

    Templates response handler functional tests completed.

--- tests/functional/Templates/TemplatesResponseHandlerTest.php
+++ tests/functional/Templates/TemplatesResponseHandlerTest.php
@@ -4,24 +4,75 @@ use Mockery as m;
 
 class TemplatesResponseHandlerTest extends \DeviseTestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->Framework = new \Devise\Support\Framework;
+        $this->Framework->Redirect = m::mock('\Illuminate\Routing\Redirector');
+
+        $this->TemplatesManager = m::mock('Devise\Templates\TemplatesManager');
+        $this->TemplatesResponseHandler = new TemplatesResponseHandler($this->TemplatesManager, $this->Framework);
+    }
+
     public function test_it_can_execute_store()
     {
-        $this->markTestIncomplete();
+        $this->TemplatesManager->shouldReceive('storeTemplate')->andReturnSelf();
+
+        $this->Framework->Redirect
+            ->shouldReceive('route')
+            ->andReturnSelf()
+            ->shouldReceive('with')
+            ->andReturn('Template registered successfully');
+
+        $result = $this->TemplatesResponseHandler->executeStore(['foo' => 'fooValue']);
+
+        assertEquals('Template registered successfully', $result);
     }
 
     public function test_it_can_execute_update()
     {
-        $this->markTestIncomplete();
+        $this->TemplatesManager->shouldReceive('updateTemplate')->andReturnSelf();
+
+        $this->Framework->Redirect
+            ->shouldReceive('route')
+            ->andReturnSelf()
+            ->shouldReceive('with')
+            ->andReturn('Template updated successfully');
+
+        $result = $this->TemplatesResponseHandler->executeUpdate('foo.templatePath', ['foo' => 'fooValue']);
+
+        assertEquals('Template updated successfully', $result);
     }
 
     public function test_it_can_execute_destroy()
     {
-        $this->markTestIncomplete();
+        $this->TemplatesManager->shouldReceive('destroyTemplate')->andReturnSelf();
+
+        $this->Framework->Redirect
+            ->shouldReceive('route')
+            ->andReturnSelf()
+            ->shouldReceive('with')
+            ->andReturn('Template deleted successfully');
+
+        $result = $this->TemplatesResponseHandler->executeDestroy('foo.template.path');
+
+        assertEquals('Template deleted successfully', $result);
     }
 
     public function test_it_can_execute_variable_store()
     {
-        $this->markTestIncomplete();
+        $this->TemplatesManager->shouldReceive('storeNewVariable')->andReturnSelf();
+
+        $this->Framework->Redirect
+            ->shouldReceive('route')
+            ->andReturnSelf()
+            ->shouldReceive('with')
+            ->andReturn('Variable successfully created');
+
+        $result = $this->TemplatesResponseHandler->executeVariableStore('foo.template.path', ['foo' => 'input']);
+
+        assertEquals('Variable successfully created', $result);
     }
 
 }

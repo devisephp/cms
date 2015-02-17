23890e191f8a206d240f57fd11b21782603bdbd5
Mauro Velez <mauro@lbm.co>
2015-02-17 10:27:31 -0600

    using dep-injected Filesystem instead of File facade in FileManager.php. adjusted tests as well

--- src/Devise/Support/Config/FileManager.php
+++ src/Devise/Support/Config/FileManager.php
@@ -66,13 +66,13 @@ class FileManager
         // set path to published config location
         $path = config_path() . '/devise';
 
-        if(!\File::isDirectory($path)) {
-            \File::makeDirectory($path,  755, true);
+        if(!$this->files->isDirectory($path)) {
+            $this->files->makeDirectory($path,  0755, true);
         }
 
         $file = $path . "/devise.{$filename}.php";
 
-        if(\File::isDirectory($path) ) {
+        if($this->files->isDirectory($path) ) {
             return $file;
         }
 

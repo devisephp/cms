2bc46e5983bb9096d648d11cfafd2d42d85eec51
Chad Lockett <chad@lbm.co>
2015-02-18 13:09:39 -0500

    Forgot to add timestamps to create users migration. Added logic to make sure column order was consistent.

--- src/migrations/2014_08_29_160748_create_users.php
+++ src/migrations/2014_08_29_160748_create_users.php
@@ -24,6 +24,7 @@ class CreateUsers extends Migration {
                 $table->string('remember_token', 255)->nullable();
                 $table->boolean('activated')->default(false);
                 $table->boolean('activate_code')->nullable();
+                $table->timestamps();
                 $table->softDeletes();
             });
 
@@ -32,15 +33,15 @@ class CreateUsers extends Migration {
             // check and create any required Devise columns
             Schema::table('users', function($table) {
                 if(!Schema::hasColumn('users', 'name')) {
-                    $table->string('name', 255)->nullable();
+                    $table->string('name', 255)->after('id')->nullable();
                 }
 
                 if(!Schema::hasColumn('users', 'email')) {
-                    $table->string('email', 255)->unique();
+                    $table->string('email', 255)->after('name')->unique();
                 }
 
                 if(!Schema::hasColumn('users', 'password')) {
-                    $table->string('password', 255);
+                    $table->string('password', 255)->after('email');
                 }
 
                 if(!Schema::hasColumn('users', 'remember_token')) {
@@ -48,11 +49,19 @@ class CreateUsers extends Migration {
                 }
 
                 if(!Schema::hasColumn('users', 'activated')) {
-                    $table->boolean('activated')->default(false);
+                    $table->boolean('activated')->after('remember_token')->default(false);
                 }
 
                 if(!Schema::hasColumn('users', 'activate_code')) {
-                    $table->boolean('activate_code')->nullable();
+                    $table->boolean('activate_code')->after('activated')->nullable();
+                }
+
+                if(!Schema::hasColumn('users', 'created_at')) {
+                    $table->timestamp('created_at')->after('activate_code');
+                }
+
+                if(!Schema::hasColumn('users', 'updated_at')) {
+                    $table->timestamp('updated_at')->after('created_at');
                 }
 
                 if(!Schema::hasColumn('users', 'deleted_at')) {

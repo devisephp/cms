729b1d4ebb020b9287230788d0669af3305ed4a1
Chad Lockett <chad@lbm.co>
2015-02-18 11:48:51 -0500

    Updated users table migrations to ensure it works with L5 users table migs.

--- src/migrations/2014_08_29_160748_create_users.php
+++ src/migrations/2014_08_29_160748_create_users.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateUsers extends Migration {
@@ -11,18 +12,55 @@ class CreateUsers extends Migration {
      */
     public function up()
     {
-        Schema::create('users', function($table) {
-            $table->increments('id');
-            $table->string('name', 255)->nullable();
-            $table->string('email', 255);
-            $table->string('password', 255);
-            $table->boolean('activated')->default(false);
-            $table->string('activate_code', 255)->nullable();
-            $table->string('remember_token', 255)->nullable();
-            $table->timestamp('created_at')->default('0000-00-00 00:00:00');
-            $table->timestamp('updated_at')->default('0000-00-00 00:00:00');
-            $table->timestamp('deleted_at')->nullable();
-        });
+        if (!Schema::hasTable('users'))
+        {
+
+            // create new
+            Schema::create('users', function( $table) {
+                $table->increments('id');
+                $table->string('name', 255)->nullable();
+                $table->string('email', 255)->unique();
+                $table->string('password', 255);
+                $table->string('remember_token', 255)->nullable();
+                $table->boolean('activated')->default(false);
+                $table->boolean('activate_code')->nullable();
+                $table->softDeletes();
+            });
+
+        } else {
+
+            // check and create any required Devise columns
+            Schema::table('users', function($table) {
+                if(!Schema::hasColumn('users', 'name')) {
+                    $table->string('name', 255)->nullable();
+                }
+
+                if(!Schema::hasColumn('users', 'email')) {
+                    $table->string('email', 255)->unique();
+                }
+
+                if(!Schema::hasColumn('users', 'password')) {
+                    $table->string('password', 255);
+                }
+
+                if(!Schema::hasColumn('users', 'remember_token')) {
+                    $table->rememberToken();
+                }
+
+                if(!Schema::hasColumn('users', 'activated')) {
+                    $table->boolean('activated')->default(false);
+                }
+
+                if(!Schema::hasColumn('users', 'activate_code')) {
+                    $table->boolean('activate_code')->nullable();
+                }
+
+                if(!Schema::hasColumn('users', 'deleted_at')) {
+                    $table->softDeletes();
+                }
+            });
+
+        }
     }
 
     /**

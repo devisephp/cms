92d4b0ffa929efa9fc87f276b2b27e170eec98b2
Chad Lockett <chad@lbm.co>
2015-02-19 17:37:05 -0500

    Password recovery system has base functionality and has been integrated into the current login system; may still need a little style love.

--- src/views/admin/users/forgot-password.blade.php
+++ src/views/admin/users/forgot-password.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     <div class="container pt sp45">
-
         <div class="tac mb sp75">
             <img src="<?= URL::asset('/packages/devisephp/cms/img/admin-logo.png') ?>">
         </div>
@@ -15,30 +14,25 @@
         </div>
         @endif
 
+        <h4 class="tac mb sp15">Forgot Password</h4>
+
         <div class="row">
             <div class="col-md-4 col-md-offset-4 tac">
-                <?= Form::open(array('method' => 'POST', 'route' => 'user-attempt-login')) ?>
-
-                    @if(URL::previous() && Request::url() != URL::previous())
-                        <input type="hidden" name="intended" value="<?= URL::previous() ?>">
-                    @endif
+                <form method="POST" action="<?= URL::route('user-attempt-recover') ?>">
+                    <input type="hidden" name="_token" value="<?= csrf_token() ?>">
 
                     <div class="form-group">
                         <input name="email" type="text" class="form-control" placeholder="Email" value="<?= old('email') ?>" />
                     </div>
 
                     <div class="form-group">
-                        <input name="password" type="password" class="form-control" placeholder="Password" />
+                        <button class="dvs-button dvs-button-primary">Send Recovery Email</button>
                     </div>
 
                     <div class="form-group">
-                        <button class="dvs-button dvs-button-primary">Log In to Administration</button>
+                        <a href="<?= URL::route('user-login') ?>">Back to Login</a>
                     </div>
-
-                    <div class="form-group">
-                        <a href="<?= URL::route('user-recover-password') ?>">Forgot Your Password?</a>
-                    </div>
-                <?= Form::close() ?>
+                </form>
             </div>
         </div>
     </div>
--- src/views/emails/recover-password.blade.php
+++ src/views/emails/recover-password.blade.php
@@ -6,23 +6,23 @@
 			<td>
 				<table>
 					<tr>
-						<td><p style="font-size:24px;margin:10px 0 4px 0;">Request for Password Reset</p></td>
+						<td><p style="font-size:24px;margin:10px 0 4px 0;">DevisePHP Password Reset</p></td>
 					</tr>
 					<tr>
 						<td style="color:#3e3e3e;font-size:14px">
 							<p>A password reset was requested for this email. Click the button to proceed with the password recovery process.</p>
 
-							<p>If you did not request a password reset, please contact support.</p>
-
 							<table style="background-color:#1FA7DB;height:75px;padding:20px;cursor:pointer;">
 								<tr>
 									<td cellpadding="20" style="text-align:center">
-										<a href="<?= URL::action('DeviseUserController@resetPassword', array($user->id)) ?>" style="color:#ffffff;text-decoration:none;font-size:24px;padding:25px 40px;">Go To Change Password</a>
+										<a href="<?= URL::route('user-reset-password') . '?token=' . $token ?>" style="color:#ffffff;text-decoration:none;font-size:24px;padding:25px 40px;">Go To Change Password</a>
 									</td>
 								</tr>
 							</table>
 
-							<p style="font-size:12px;padding-top:10px;">If the button above isn't working, paste the following link into your browser: <a href="<?= URL::action('DeviseUserController@resetPassword', array($user->id)) ?>" style="color:#15c;text-decoration:none;"><?= URL::action('DeviseUserController@resetPassword', array($user->id)) ?></a></p>
+							<p style="font-size:12px;padding-top:10px;">If the button above isn't working, paste the following link into your browser: <a href="<?= URL::route('user-reset-password') . '?token=' . $token ?>" style="color:#15c;text-decoration:none;"><?= URL::route('user-reset-password') . '?token=' . $token ?></a></p>
+
+                            <p>If you did not request a password reset, please contact support.</p>
 						</td>
 					</tr>
 				</table>

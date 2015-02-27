63f58bc4390e1ed399006e54bb74f5b3c44eb55b
Chad Lockett <chad@lbm.co>
2015-02-27 18:28:10 -0500

    Added content-requested to init. select options for dvd-groups-select.

--- src/views/admin/sidebar/_sidebar-groups-select.blade.php
+++ src/views/admin/sidebar/_sidebar-groups-select.blade.php
@@ -1,10 +1,19 @@
 @if(!$data->isCollection)
     @if ($data->groups && count($data->groups) >= 2)
         <div id="dvs-sidebar-groups">
-            <select class="dvs-select" name="groups" class="large">
+            <select class="dvs-select" id="dvs-groups-select" name="groups" class="large">
                 @php $index = 0 @endphp
+
                 @foreach ($data->groups as $groupName => $group)
-                    <option value="<?= $index++ ?>">Group: <?= $groupName ?></option>
+                    @php $contentRequested = false; @endphp
+
+                    @foreach($group as $element)
+                        @if($element->content_requested)
+                            @php $contentRequested = true;  @endphp
+                        @endif
+                    @endforeach
+
+                    <option class="<?= ($contentRequested) ? 'dvs-content-request' : '' ?>" value="<?= $index++ ?>">Group: <?= $groupName ?></option>
                 @endforeach
             </select>
         </div>
@@ -12,17 +21,26 @@
 @else
     @if ($data->groups)
         <div id="dvs-sidebar-groups">
-            <select class="dvs-select dvs-button-solid" name="groups" class="large">
+            <select class="dvs-select dvs-button-solid" id="dvs-groups-select" name="groups" class="large">
                 @php $index = 0 @endphp
-                @foreach ($data->groups as $groupName => $group)
-                    <option value="<?= $index++ ?>"><?= $groupName ?></option>
+
+                @foreach($data->groups as $groupName => $group)
+                    @php $contentRequested = false; @endphp
+
+                    @foreach($group as $element)
+                        @if($element->content_requested)
+                            @php $contentRequested = true;  @endphp
+                        @endif
+                    @endforeach
+
+                    <option class="<?= ($contentRequested) ? 'dvs-content-request' : '' ?>" value="<?= $index++ ?>"><?= $groupName ?></option>
                 @endforeach
             </select>
             <button id="dvs-sidebar-manage-groups" class="dvs-button dvs-button-secondary dvs-button-small dvs-button-solid">Collections</button>
         </div>
     @else
         <div id="dvs-sidebar-groups">
-            <select class="dvs-select" name="groups" class="large">
+            <select class="dvs-select" id="dvs-groups-select" name="groups" class="large">
                 <option value="0">None</option>
             </select>
             <button id="dvs-sidebar-manage-groups" class="dvs-button dvs-button-secondary dvs-button-small dvs-button-solid">Collections</button>

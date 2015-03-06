7014accc0f86efb67a89dd7babed2cf12522f7ab
Mauro Velez <mauro@lbm.co>
2015-03-06 15:22:27 -0600

    wrapped the installer in our 'public' styles

--- src/views/installer/welcome.blade.php
+++ src/views/installer/welcome.blade.php
@@ -1,18 +1,10 @@
 @extends('devise::layouts.installer')
 
 @section('content')
+    <h1>Thank You &amp; Welcome</h1>
 
-    <img src="<?= url('packages/devisephp/cms/img/devise-installer-logo.gif') ?>" width="300" height="300">
-
-    <div class="main-content">
-
-	    <h1>Thank You &amp; Welcome</h1>
-
-	    <p>
-	        We want to thank you for taking the time to give Devise a spin. We know that, as a developer, you have the opportunity to try out many packages and solutions for your team and clients. If there is anything we can do to help or if you have any feedback please reach out to us at <a href="http://devisephp.com" target="_blank">Devisephp.com</a>.<br><br>
-	    	<a class="get-started dvs-button dvs-button-success dvs-button-solid" href="environment">Get Started With Install</a>
-	    </p>
-
-	</div>
-
+    <p>
+        We want to thank you for taking the time to give Devise a spin. We know that, as a developer, you have the opportunity to try out many packages and solutions for your team and clients. If there is anything we can do to help or if you have any feedback please reach out to us at <a href="http://devisephp.com" target="_blank">Devisephp.com</a>.<br><br>
+    	<a class="get-started dvs-button dvs-button-success dvs-button-solid" href="environment">Get Started With Install</a>
+    </p>
 @stop

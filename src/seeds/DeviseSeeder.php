43bba3bf68beae9dfe5ee817da180c4618634f23
Chad Lockett <chad@lbm.co>
2015-03-09 16:27:04 -0400

    Removed DeviseAdminUserSeeder so no user record is created by default.

--- src/seeds/DeviseSeeder.php
+++ src/seeds/DeviseSeeder.php
@@ -14,7 +14,6 @@ class DeviseSeeder extends \Devise\Support\DeviseSeeder
 		// these things only need to be run once
 		// the admin is able to change them afterwards
 		$this->callOnlyOnce('DeviseLanguagesSeeder');
-		$this->callOnlyOnce('DeviseAdminUserSeeder');
 
 		// pages is wired so that it will allow us
 		// to keep cramming pages in there if we

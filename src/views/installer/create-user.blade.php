773db735ca3409d53ef4c8ec8ca1194be8af3aaa
Chad Lockett <chad@lbm.co>
2015-03-09 16:42:35 -0400

    Quick style adjustment to create database and create user forms for installer.

--- src/views/installer/create-user.blade.php
+++ src/views/installer/create-user.blade.php
@@ -7,22 +7,22 @@
     <form method="post">
         <input type="hidden" name="_token" value="<?= csrf_token() ?>">
 
-        <div class="dvs-form-group">
+        <div class="form-group">
             <label>Admin Email</label>
             <input type="text" class="form-control" name="email" placeholder="Admin Email" value="<?= $email ?>">
         </div>
 
-        <div class="dvs-form-group">
+        <div class="form-group">
             <label>Admin Username</label>
             <input type="text" class="form-control" name="username" placeholder="Admin Username" value="<?= $username ?>">
         </div>
 
-        <div class="dvs-form-group">
+        <div class="form-group">
             <label>Admin Password</label>
             <input type="text" class="form-control" name="password" placeholder="Admin Password" value="">
         </div>
 
-        <div class="dvs-form-group dvs-form-actions mt sp30">
+        <div class="form-group dvs-form-actions mt sp30">
             <button class="back dvs-button dvs-button-secondary" type="button" onclick="location.href='database'">Back</button>
             <button class="next dvs-button dvs-button-success">Next</button>
         </div>

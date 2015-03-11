498123cf60941211f3958050cda426c20dd090a3
Kelt Dockins <kelt@dockins.org>
2015-03-11 16:20:16 -0500

    [DVS-158] writing tests for incompletes for doc system

--- tests/functional/Pages/Docs/CreatorTest.php
+++ tests/functional/Pages/Docs/CreatorTest.php
@@ -1,16 +1,26 @@
-<?php namespace Devise\Docs\Pages;
+<?php namespace Devise\Pages\Docs;
 
 use Mockery as m;
 
 class CreatorTest extends \DeviseTestCase
 {
-	public function test_it_can_create_devise_docs_for_view()
+	public function setUp()
 	{
-		$this->markTestIncomplete();
+		parent::setUp();
+		$this->Framework = new \Devise\Support\Framework;
+		$this->LiveSpan = m::mock('Devise\Pages\Docs\LiveSpan');
+		$this->CommonMarkConverter = m::mock('League\CommonMark\CommonMarkConverter');
+		$this->Creator = new Creator($this->LiveSpan, $this->Framework, $this->CommonMarkConverter);
 	}
 
-	public function test_it_can_create_devise_docs_for_missing_view()
+	public function test_it_can_create_devise_docs_for_view()
 	{
-		$this->markTestIncomplete();
+		$content = 'some content';
+		$this->Creator->View = m::mock('SomeViewObj');
+		$this->Creator->View->shouldReceive('make')->twice()->andReturnSelf();
+		$this->Creator->View->shouldReceive('render')->twice()->andReturn($content);
+		$this->CommonMarkConverter->shouldReceive('convertToHtml')->once()->with($content)->andReturn($content);
+		$this->LiveSpan->shouldReceive('replace')->once()->with($content)->andReturn($content);
+		assertContains($content, $this->Creator->deviseDocs('devise::some.view.path'));
 	}
 }
--- tests/functional/Support/StrTest.php
+++ tests/functional/Support/StrTest.php
@@ -2,13 +2,10 @@
 
 class StrTest extends \DeviseTestCase
 {
-    public function test_it_replaces_between_haystack_without_closure()
-    {
-        $this->markTestIncomplete();
-    }
-
     public function test_it_replaces_between_haystack_with_closure()
     {
-        $this->markTestIncomplete();
+    	$Str = new Str;
+        $outcome = $Str->replaceBetween('something @here(...) and @here(...)', '@here', function($between) { return "[ $between ]"; });
+        assertEquals('something [ ... ] and [ ... ]', $outcome);
     }
 }

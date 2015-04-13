2928426c18b190de01eaae37240280de6d2f69d7
Mauro Velez <mauro@lbm.co>
2015-04-13 15:33:44 -0400

    WIP: acceptance testing. 'create new user' node

--- tests/acceptance/CreateNewUserCest.php
+++ tests/acceptance/CreateNewUserCest.php
@@ -6,6 +6,8 @@ class CreateNewUserCest extends BaseTester {
     {
         $this->login($I);
         $this->gotToFieldsAndShowNodes($I);
+
+        $this->clickNode($I, 'Create New User');
     }
 
     public function _after(AcceptanceTester $I)
@@ -16,19 +18,35 @@ class CreateNewUserCest extends BaseTester {
     /**
      * clicking on the node and open sidebar
      */
-    public function clickOnNodeToOpenSidebar(AcceptanceTester $I)
+    public function checkTheGridIsThere(AcceptanceTester $I)
     {
-        $this->clickNode($I, 'Create New User');
+        $I->seeElement('button[data-creator-attribute-name="Name"]');
+        $I->seeElement('button[data-creator-attribute-name="Email"]');
+        $I->seeElement('button[data-creator-attribute-name="Password"]');
     }
 
     /**
-     * clicking on the "name" grid item
+     * clicking on the all the grid items and using the bread crumbs
      */
     public function clickEachGridItem(AcceptanceTester $I)
     {
-        $this->clickNode($I, 'Create New User');
+        $I->click('button[data-creator-attribute-name="Name"]');
+        $I->waitForElement('#dvs-sidebar-field-form', self::WAIT_TIME);
 
-        $I->click('.dvs-sidebar-elements button');
+        $I->see('Max Length');
+        $I->click('a[data-breadcrumb-id="0"]');
+        $I->wait(1);
+        $I->dontSee('Max Length');
+
+        $I->click('button[data-creator-attribute-name="Email"]');
+        $I->waitForElement('#dvs-sidebar-field-form', self::WAIT_TIME);
+
+        $I->see('Max Length');
+        $I->click('a[data-breadcrumb-id="0"]');
+        $I->wait(1);
+        $I->dontSee('Max Length');
+
+        $I->click('button[data-creator-attribute-name="Password"]');
         $I->waitForElement('#dvs-sidebar-field-form', self::WAIT_TIME);
 
         $I->see('Max Length');
@@ -38,22 +56,47 @@ class CreateNewUserCest extends BaseTester {
     }
 
     /**
-     * fill out field and save, refresh and make sure random value is on page
+     * click on name and then try to save with blank form
+     */
+    public function checkValidationFails(AcceptanceTester $I)
+    {
+        $I->click('button[data-creator-attribute-name="Name"]');
+        $I->waitForElement('#dvs-sidebar-field-form', self::WAIT_TIME);
+
+        $I->click('button.dvs-sidebar-save-group');
+
+        $I->waitForElement('#dvs-sidebar-validation-errors', 20); // waiting for ajax to return
+        $I->see('field is required');
+    }
+
+    /**
+     * fill out field and save, refresh and make sure the value is saved
      */
     public function saveTestValue(AcceptanceTester $I)
     {
-        $value = $this->randString();
-
-        $this->clickNode($I, 'Edit the Page Versioned Model');
-        
-        $I->click('.dvs-sidebar-elements button');
+        $I->click('button[data-creator-attribute-name="Name"]');
         $I->waitForElement('#dvs-sidebar-field-form', self::WAIT_TIME);
 
-        $I->fillField('text',$value);
+        $I->fillField('text','tester');
+        $I->click('a[data-breadcrumb-id="0"]');
+        $I->wait(1);
+
+        $I->click('button[data-creator-attribute-name="Email"]');
+        $I->waitForElement('#dvs-sidebar-field-form', self::WAIT_TIME);
+
+        $I->fillField('text','testemail@lbm.co');
+        $I->click('a[data-breadcrumb-id="0"]');
+        $I->wait(1);
+
+        $I->click('button[data-creator-attribute-name="Password"]');
+        $I->waitForElement('#dvs-sidebar-field-form', self::WAIT_TIME);
+
+        $I->fillField('text','password');
+
         $I->click('button.dvs-sidebar-save-group');
 
         $I->wait(5); // waiting for ajax to save
-        $I->seeInDatabase('dvs_test_models', array('id' => 1, 'name' => $value));
+        $I->seeInDatabase('users', array('email' => 'testemail@lbm.co'));
     }
 }
 ?>

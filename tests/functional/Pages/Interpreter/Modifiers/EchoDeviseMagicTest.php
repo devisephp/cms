d8707f4439e327c88274ed70a39c1343167b1927
Kelt Dockins <kelt@dockins.org>
2015-05-11 15:25:22 -0500

    making devisemagic cleaner (removing devisemagicstart and finish)

--- tests/functional/Pages/Interpreter/Modifiers/EchoDeviseMagicTest.php
+++ tests/functional/Pages/Interpreter/Modifiers/EchoDeviseMagicTest.php
@@ -16,33 +16,21 @@ class EchoDeviseMagicTest extends \DeviseTestCase
 		assertCount(6, $nodes);
 
 		// dd($nodes[0]);
-		assertEquals('startdvsmagic', $nodes[0]->exprs[0]->left->left->name);
-		assertEquals('dvsmagic', $nodes[0]->exprs[0]->left->right->name);
-		assertEquals('enddvsmagic', $nodes[0]->exprs[0]->right->name);
+		assertEquals('dvsmagic', $nodes[0]->exprs[0]->name);
 
 		// dd($nodes[1]);
-		assertEquals('startdvsmagic', $nodes[1]->exprs[0]->left->left->name);
-		assertEquals('dvsmagic', $nodes[1]->exprs[0]->left->right->name);
-		assertEquals('enddvsmagic', $nodes[1]->exprs[0]->right->name);
+		assertEquals('dvsmagic', $nodes[1]->exprs[0]->name);
 
 		// dd($nodes[2]);
-		assertEquals('startdvsmagic', $nodes[2]->exprs[0]->left->left->name);
-		assertEquals('dvsmagic', $nodes[2]->exprs[0]->left->right->name);
-		assertEquals('enddvsmagic', $nodes[2]->exprs[0]->right->name);
+		assertEquals('dvsmagic', $nodes[2]->exprs[0]->name);
 
 		// dd($nodes[3]);
-		assertEquals('startdvsmagic', $nodes[3]->exprs[0]->if->left->left->name);
-		assertEquals('dvsmagic', $nodes[3]->exprs[0]->if->left->right->name);
-		assertEquals('enddvsmagic', $nodes[3]->exprs[0]->if->right->name);
+		assertEquals('dvsmagic', $nodes[3]->exprs[0]->if->name);
 
 		// dd($nodes[4]);
-		assertEquals('startdvsmagic', $nodes[4]->exprs[0]->left->left->left->left->name);
-		assertEquals('dvsmagic', $nodes[4]->exprs[0]->left->left->left->right->name);
-		assertEquals('enddvsmagic', $nodes[4]->exprs[0]->left->left->right->name);
+		assertEquals('dvsmagic', $nodes[4]->exprs[0]->left->left->name);
 
 		// dd($nodes[5]);
-		assertEquals('startdvsmagic', $nodes[5]->exprs[0]->args[0]->value->left->left->name);
-		assertEquals('dvsmagic', $nodes[5]->exprs[0]->args[0]->value->left->right->name);
-		assertEquals('enddvsmagic', $nodes[5]->exprs[0]->args[0]->value->right->name);
+		assertEquals('dvsmagic', $nodes[5]->exprs[0]->args[0]->value->name);
 	}
 }

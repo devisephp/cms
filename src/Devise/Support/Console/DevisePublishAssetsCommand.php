c9aea43a2de9d9160b630435bb910c1c2f01252c
Kelt Dockins <kelt@dockins.org>
2015-05-13 11:24:52 -0500

    [DVS-234] error pages for 404s

--- src/Devise/Support/Console/DevisePublishAssetsCommand.php
+++ src/Devise/Support/Console/DevisePublishAssetsCommand.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Console\Command;
 use Illuminate\Container\Container;
+use File;
 
 class DevisePublishAssetsCommand extends Command
 {
@@ -24,6 +25,27 @@ class DevisePublishAssetsCommand extends Command
      */
     public function handle()
     {
-        \File::copyDirectory(__DIR__ . '/../../../../public', public_path() . '/packages/devisephp/cms');
+        File::copyDirectory(__DIR__ . '/../../../../public', public_path() . '/packages/devisephp/cms');
+
+        $this->copyErrorViewsToApplication();
+    }
+
+    /**
+     * Copies the error pages over to the application
+     *
+     * @return void
+     */
+    protected function copyErrorViewsToApplication()
+    {
+        $views = base_path() . '/resources/views/errors/';
+
+        if (! File::isDirectory($views)) File::makeDiretory($views, 511, true);
+
+        foreach (File::files(__DIR__ . '/../../../views/errors') as $file)
+        {
+            $errorfile = $views . basename($file);
+
+            if (! File::exists($errorfile)) File::copy($file, $errorfile);
+        }
     }
 }

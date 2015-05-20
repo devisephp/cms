c233df08ed0f300f13ef8905bdf15f9a064e8070
Chad Lockett <chad@lbm.co>
2015-05-20 11:38:17 -0400

    Added additional docs content for templates edit.

--- src/views/docs/admin/templates/edit.php
+++ src/views/docs/admin/templates/edit.php
@@ -1,40 +1,84 @@
 # Editing a Template
 
-### Important Notes
-You will notice the file name dropdown has been populated by Devise, but you might be wondering where and why? Devise checks inside the ```/resources/views/``` directory (and any sub-directories) for any non-registered blade files which *do not begin with an underscore (_)*. These are excluded becuase in Devise partials start with an underscore and can be included inside multiple templates.
+### Getting Devise to Recognize Your Template
+The file name dropdown is auto-populated by Devise, but you might be wondering where and why? Devise checks the ```/resources/views/``` directory and it's sub-directories for any unregistered blade files which *do not begin with an underscore (_)*. These files are excluded because partials start with an underscore and can be included inside multiple templates.
+
+*The current template's path is:*
+
+``` php
+/resources/views/@livespan([name="template_path"]).blade.php
+```
 
 ### Human Name Field
 
-The human name field is used on the admin templates.
+Primarily used as meta data on the admin templates index.
 
 ### Extends Field
 
-This sets the layout used by the template blade and the value is inserted into:
+Defines layout used by the template and the value is inserted into the extends tag within the blade file:
+
 ``` php
-@extends('devise::admin.layouts.master')
+@extends(@livespan([name="template[extends]"]))
 ```
 
 ### Variables
 
-Variables offer a quick and easy way to manage data accessible within a template. Under the "Variables" section are two lists of variables. The first, is any variables already defined and set for the current template. The second, is any variables found within the template's blade file but have not been added and saved to the current template. These "undefined" variables will only be attempted to be added when you input values in the Class Path and Method Name fields, and then click Update Template.
+Template variables offer a simple and quick way to set data available inside a template. All defined variables are listed in the "Variables" section.
 
-### Adding New Variable(s)
+### Adding New Variables
 
 After clicking on the "Add New Variable" button, a variable can be created in two ways:
-1. Select an existing variable in Devise (this list is pulled from all the other registered templates).
+1. Select an existing variable from the list.
 1. Add a new variable. This requires three fields:
 
 #### Variable Name
-Any unique variable name which will be available in the current template.
+Any unique variable name which will become available in the current template.
 
 #### Class Path
-The namespaced class path where you will be retrieving data.
+The namespaced class where you will be retrieving data.
 
 #### Method Name
-The method/function name which will be called in the above class.
+The method/function name which will be called from inside the Class Path set above.
 
 ### Adding Param(s) to Variables
 
-After a new variable has been successfully added to the template being editted, you will notice a button labeled "Add Param." This opens a modal where different parameters can be added to support the method being called.
+After a new variable has been added to the current template, parameters can be added with the "Create New Param" form. Currently, Devise supports these parameter types:
+- Input
+- Url Parameters
+- Defined Variables (any vars already added to current template)
+- Static Values
 
-#### Param Types
+### Parameter Examples
+
+#### Input Param Examples
+Assume our url looks like, __<?= url() ?>?fooId=10&isLoggedIn=1__, which calls the *_fooIsUserLoggedIn_* method inside of class Foo\UserManager.
+
+__input__ - passes the entire query input to the method.
+
+```
+public function fooIsUserLoggedIn($input)
+{
+    var_dump($input); // array('fooId' => 10, isLoggedIn' => 1)
+}
+```
+
+__input.fooId__ - only passes "fooId" from the query input array
+
+```
+public function fooIsUserLoggedIn($fooId)
+{
+    echo $fooId; // 10
+}
+```
+
+#### URL Param Example
+
+Now, our url has a param for "id" __<?= url("/some/:id") ?>__ and for our example let's say the id is "10". Now, we can get the id like:
+
+__params.id__ - passes the id
+```
+public function fooFindUserById($id)
+{
+    echo $id;   // 10
+}
+```

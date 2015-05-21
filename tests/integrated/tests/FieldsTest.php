0bc4a0e9761f47efc2ab62bae64b077df19d8821
Kelt Dockins <kelt@dockins.org>
2015-05-21 15:28:57 -0500

    got the first test working boooyah...

--- tests/integrated/tests/FieldsTest.php
+++ tests/integrated/tests/FieldsTest.php
@@ -2,8 +2,16 @@
 
 class FieldsTest extends Integrated
 {
-	public function test_it_does_thing()
+	public function test_it_can_edit_the_page_version_model()
 	{
-		$this->visit('/');
+		$text = str_random(32);
+		$this->sidebar('model0-node')
+			 ->mousedown('[data-model-field-id]')
+			 ->clear('[name=text]')
+			 ->fill($text, 'text')
+			 ->mousedown('.dvs-sidebar-save-group')
+			 ->wait()
+			 ->seeInDatabase('dvs_model_fields', ['json_value' => '{"text":"'.$text.'","maxlength":""}']);
 	}
+
 }
--- tests/integrated/tests/Integrated.php
+++ tests/integrated/tests/Integrated.php
@@ -2,8 +2,76 @@
 
 use Laracasts\Integrated\Extensions\Selenium;
 use Laracasts\Integrated\Services\Laravel\Application as Laravel;
-
+use Laracasts\Integrated\Extensions\Traits\WorksWithDatabase as Database;
 class Integrated extends Selenium
 {
 	use Laravel;
+	use Database;
+
+	/**
+	 * Login to the devise admin section to some page
+	 *
+	 * @param  [type] $toPage
+	 * @return [type]
+	 */
+	protected function login($toPage)
+	{
+		$this->visit($toPage)
+			 ->seePageIs('/admin/login')
+			 ->type('devise', 'uname_or_email')
+			 ->type('password', 'password')
+			 ->press('Log In to Administration');
+
+		return $this;
+	}
+
+	/**
+	 * Open the sidebar for a given field
+	 *
+	 * @param  [type] $field
+	 * @return [type]
+	 */
+	protected function sidebar($field)
+	{
+		$this->login('/admin/fields')
+			 ->waitForElement('#dvs-node-mode-button')
+			 ->findByNameOrId('#dvs-node-mode-button')
+			 ->click();
+
+		$this->session->frame(['id' => 'dvs-iframe']);
+
+		$this->waitForElement($field)
+			 ->findByNameOrId($field)
+			 ->click();
+
+		$this->session->frame(['id' => null]);
+
+		return $this;
+	}
+
+	/**
+	 * Overrides the click because it is very
+	 * limited on what we can select on
+	 *
+	 * @param  [type] $selector
+	 * @return [type]
+	 */
+	protected function mousedown($selector, $timeout = 5000)
+	{
+		$this->session->timeouts()->postImplicit_wait(['ms' => $timeout]);
+		$this->session->element('css selector', $selector)->click();
+		return $this;
+	}
+
+	/**
+	 * [clear description]
+	 * @param  [type] $selector
+	 * @return [type]
+	 */
+	protected function clear($selector, $timeout = 5000)
+	{
+		$this->session->timeouts()->postImplicit_wait(['ms' => $timeout]);
+		$this->session->element('css selector', $selector)->clear();
+		return $this;
+	}
 }

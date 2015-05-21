2c69fcc8f7067ed8ee930f6e87552a13b8662817
Kelt Dockins <kelt@dockins.org>
2015-05-21 13:22:04 -0500

    removing vendor directory

--- tests/integrated/vendor/masterminds/html5/src/HTML5/Exception.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace HTML5;
-
-/**
- * The base exception for the HTML5 project.
- */
-class Exception extends \Exception {
-}
--- tests/integrated/vendor/masterminds/html5/src/HTML5/Parser/ParseError.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace HTML5\Parser;
-
-/**
- * Emit when the parser has an error.
- */
-class ParseError extends \Exception {
-}

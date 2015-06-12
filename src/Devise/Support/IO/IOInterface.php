78ec7d4a3cda780f2a79910aac0cb975802eb711
Chad Lockett <chad@lbm.co>
2015-06-12 10:21:05 -0400

    Updated IOInterface table method to match Illuminate Command table method.

--- src/Devise/Support/IO/IOInterface.php
+++ src/Devise/Support/IO/IOInterface.php
@@ -7,7 +7,7 @@ interface IOInterface
 	public function askWithCompletion($question, array $choices, $default = null);
 	public function secret($question, $fallback = true);
 	public function choice($question, array $choices, $default = null, $attempts = null, $multiple = null);
-	public function table(array $headers, array $rows, $style = 'default');
+	public function table(array $headers, $rows, $style = 'default');
 	public function info($string);
 	public function line($string);
 	public function comment($string);

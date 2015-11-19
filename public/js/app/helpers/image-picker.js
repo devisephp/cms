26eea197ee7013fe51e3ccb050fe9c2640e3d996
Gary Williams <gary@lbm.co>
2015-11-19 17:15:28 -0500

    Added ability to put an image tag next to any image picker and put '.image-picker-preview' on it enabling quick preview of the image selected.

--- public/js/app/helpers/image-picker.js
+++ public/js/app/helpers/image-picker.js
@@ -2,6 +2,7 @@ devise.define(['jquery'], function($) {
 	$(document).ready(function()
 	{
 		initImagePicker();
+		initImagePreview();
 	});
 
 	function initImagePicker()
@@ -9,14 +10,42 @@ devise.define(['jquery'], function($) {
 		$('body').on('click', '.image-picker', function()
 	    {
 	        var input = $(this);
-
 	        var mediaUrl = '/admin/media-manager?cropMode=Preserve&type=image';
-
 	        window.open(mediaUrl, 'Media Manager', "width=1024,height=768,location=no");
 
 	        document.onMediaManagerSelect = function(images){
+	            var preview = input.siblings('img.image-picker-preview');
+
 	            input.val(images);
+				hideOrShowPreview(preview, images);
 	        };
 	    });
+	}
+
+	function initImagePreview()
+	{
+		hideOrShowPreview();
+		addImagePickerPreviewListeners();
+	}
+
+	function hideOrShowPreview() {
+		$('.image-picker').each(function(el){
+			var preview = $(this).siblings('img.image-picker-preview');
+
+			if (preview.size() > 0) {
+				if ($(this).val() == "") {
+		    		preview.css('display', 'none')
+		    	} else {
+		    		preview.attr('src', $(this).val());
+		    		preview.css('display', 'inline-block')
+		    	}
+		    }
+		});
+	}
+
+	function addImagePickerPreviewListeners() {
+		$('body').on('click', '.image-picker-preview', function() {
+			var input = $(this).siblings('.image-picker').trigger('click');
+		});
 	}
 });

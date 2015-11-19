f595ef7b735ea3afcb66b6b724de0a983873ff22
Gary Williams <gary@lbm.co>
2015-11-19 09:51:17 -0500

    Reset image picker

--- public/js/app/helpers/image-picker.js
+++ public/js/app/helpers/image-picker.js
@@ -9,17 +9,13 @@ devise.define(['jquery'], function($) {
 		$('body').on('click', '.image-picker', function()
 	    {
 	        var input = $(this);
-	        // var image = $(this).siblings('img');
 
 	        var mediaUrl = '/admin/media-manager?cropMode=Preserve&type=image';
 
 	        window.open(mediaUrl, 'Media Manager', "width=1024,height=768,location=no");
 
-
 	        document.onMediaManagerSelect = function(images){
 	            input.val(images);
-	            // console.log(images);
-	            // input.attr('src', images);
 	        };
 	    });
 	}

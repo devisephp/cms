55cc3f287d5120bffde0ec4a101b767be0dcc955
Mauro Velez <mauro@lbm.co>
2018-04-12 09:42:52 -0400

    not formatting when null

--- Devise/Http/Resources/Api/UserResource.php
+++ Devise/Http/Resources/Api/UserResource.php
@@ -18,8 +18,8 @@ class UserResource extends Resource
       'id'         => $this->id,
       'name'       => $this->name,
       'email'      => $this->email,
-      'created_at' => $this->created_at->format('Y-m-d H:i:s'),
-      'updated_at' => $this->updated_at->format('Y-m-d H:i:s')
+      'created_at' => ($this->created_at) ? $this->created_at->format('Y-m-d H:i:s') : '',
+      'updated_at' => ($this->updated_at) ? $this->updated_at->format('Y-m-d H:i:s') : ''
     ];
   }
 }

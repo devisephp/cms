8575317b26c5ecc1ecd3a3f1e68fbad0d07c22fc
Mauro Velez <mauro@lbm.co>
2018-04-26 13:09:24 -0400

    WIP:extending the migrator

--- Devise/MotherShip/Migrations.php
+++ Devise/MotherShip/Migrations.php
@@ -3,13 +3,13 @@
 
 namespace Devise\MotherShip;
 
-
 use Illuminate\Database\Migrations\Migrator;
 
 class Migrations extends Migrator
 {
   public function getQueriesBetweenDates($start, $end)
   {
+    dd($start, $end);
     $all = [];
 
     return $all;

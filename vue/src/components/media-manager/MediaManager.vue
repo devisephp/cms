6cc6e16ba2b057bb6569c9356f2ac31e42ea733d
Gary Williams <gary@lbm.co>
2018-06-18 14:40:41 -0400

    Media manager display issues

--- vue/src/components/media-manager/MediaManager.vue
+++ vue/src/components/media-manager/MediaManager.vue
@@ -11,18 +11,23 @@
         <div class="dvs-py-4 dvs-px-8 dvs-rounded-tl dvs-rounded-tr dvs-flex dvs-justify-between dvs-items-center dvs-bg-grey-lighter dvs-border-b dvs-border-lighter dvs-relative">
           Media Manager
           <div class="dvs-flex">
-            <fieldset class="dvs-mr-4">
-              <label class="dvs-mr-2">List Mode</label>
-              <input type="checkbox" v-model="listMode">
+            <div class="dvs-flex dvs-items-center">
+              <attach-icon />
+              <vue-dropzone
+                ref="myVueDropzone"
+                id="dropzone"
+                @vdropzone-success="uploadSuccess()"
+                @vdropzone-error="uploadError"
+                class="dvs-mr-8 dvs-uppercase dvs-font-bold dvs-text-xs"
+                includeStyling: false
+                :options="dropzoneOptions" />
+            </div>
+            <fieldset>
+              <div class="dvs-flex dvs-items-center">
+                <label class="dvs-mr-2 dvs-mb-0">List Mode</label>
+                <input type="checkbox" v-model="listMode">
+              </div>
             </fieldset>
-            <vue-dropzone
-              ref="myVueDropzone"
-              id="dropzone"
-              @vdropzone-success="uploadSuccess()"
-              @vdropzone-error="uploadError"
-              class="pl-4"
-              includeStyling: false
-              :options="dropzoneOptions" />
           </div>
         </div>
 
@@ -30,43 +35,47 @@
 
           <div class="dvs-p-8 dvs-bg-grey-lightest dvs-flex dvs-flex-col dvs-justify-between dvs-border-r dvs-border-lighter dvs-min-w-1/3">
 
-            <ul class="dvs-list-reset dvs-mb-10 dvs-font-mono dvs-text-sm dvs-tracking-tight">
-              <li v-for="directory in directories" class="dvs-cursor-pointer dvs-mt-2 dvs-text-bold" @click="changeDirectories(directory.path)">
-                <i class="ion-folder dvs-mr-2"></i>
-                {{ directory.name }}
-              </li>
-              <li v-if="directories.length < 1">
-                No directories within this directory
-              </li>
-            </ul>
+            <!-- Bread Crumbs -->
+            <div>
+              <div class="dvs-flex dvs-mb-8 dvs-justify-between dvs-items-center dvs-font-mono dvs-text-sm dvs-tracking-tight"  v-if="currentDirectory !== ''">
+                <breadcrumbs :currentDirectory="currentDirectory" @chooseDirectory="changeDirectories"></breadcrumbs>
+              </div>
+
+              <ul class="dvs-list-reset dvs-mb-10 dvs-font-mono dvs-text-sm dvs-tracking-tight">
+                <li v-for="directory in directories" :key="directory.id" class="dvs-cursor-pointer dvs-mt-2 dvs-text-bold" @click="changeDirectories(directory.path)">
+                  <folder-icon class="dvs-mr-2"></folder-icon>
+                  {{ directory.name }}
+                </li>
+                <li v-if="directories.length < 1">
+                  No directories within this directory
+                </li>
+              </ul>
+            </div>
 
             <div class="dvs-flex dvs-flex-col">
               <fieldset class="dvs-fieldset dvs-mb-4">
                 <input type="text" placeholder="New Directory" v-model="directoryToCreate" class="mr-2">
               </fieldset>
-              <button class="dvs-btn dvs-btn-sm" @click="requestCreateDirectory()">Create</button>
+              <button class="dvs-btn dvs-btn-sm" @click="requestCreateDirectory()" :style="actionButtonTheme">Create</button>
             </div>
           </div>
           <div class="dvs-flex-grow dvs-relative dvs-overflow-scroll" :class="{'w-full': directories.length < 1}">
 
-            <!-- Bread Crumbs -->
-            <div class="dvs-flex dvs-justify-between dvs-my-4 dvs-items-center dvs-font-mono dvs-text-sm dvs-tracking-tight dvs-px-8 dvs-py-4"  v-if="currentDirectory !== ''">
-              <breadcrumbs :currentDirectory="currentDirectory" @chooseDirectory="changeDirectories"></breadcrumbs>
-            </div>
-
             <!-- Delete Directory -->
-            <div v-if="files.length < 1 && directories.length < 1" class="dvs-flex dvs-justify-center dvs-items-center dvs-absolute dvs-absolute-center">
-              <div class="dvs-bg-white dvs-rounded dvs-p-8 dvs--mt-15 dvs-text-center dvs-shadow dvs-cursor-pointer hover:dvs-bg-blue-dark hover:dvs-text-white" @click="requestDeleteDirectory()">
-                <i class="ion-trash-a dvs-text-5xl"></i>
-                <h6 class="dvs-mt-2"><span>Delete this directory</span></h6>
+            <div v-if="files.length < 1 && directories.length < 1 && currentDirectory !== ''" class="dvs-flex dvs-justify-center dvs-items-center dvs-absolute dvs-absolute-center">
+              <div class="dvs-bg-white dvs-rounded dvs-p-8 dvs--mt-15 dvs-text-center dvs-shadow dvs-cursor-pointer" @click="requestDeleteDirectory()">
+                <trash-icon :h="40" :w="40" :style="{color: theme.adminBackground.color}" />
+                <h6 class="dvs-mt-2 dvs-text-sm">
+                  Delete this directory
+                </h6>
               </div>
             </div>
 
             <!-- Directories but no files -->
-            <div v-if="files.length < 1 && directories.length > 1" class="dvs-flex dvs-justify-center dvs-items-center dvs-absolute dvs-absolute-center">
+            <div v-if="files.length < 1 && directories.length > 0 && currentDirectory !== ''" class="dvs-flex dvs-justify-center dvs-items-center dvs-absolute dvs-absolute-center">
               <div class="dvs-bg-white dvs-rounded dvs-p-8 dvs--mt-15 dvs-text-center dvs-shadow">
-                <i class="ion-folder dvs-text-5xl"></i>
-                <h6 class="dvs-mt-2"><span>No files in this directory</span></h6>
+                <folder-icon :h="40" :w="40" :style="{color: theme.adminBackground.color}" />
+                <h6 class="dvs-mt-2 dvs-text-sm"><span>No files in this directory</span></h6>
               </div>
             </div>
 
@@ -76,25 +85,24 @@
                 'dvs-flex dvs-flex-wrap': !listMode
               }"
              v-else>
-              <li v-for="file in files" class="dvs-relative dvs-bg-white dvs-card dvs-mt-2"
+              <li v-for="file in files" :key="file.id" class="dvs-relative dvs-bg-white dvs-card dvs-mt-2 dvs-p-4 dvs-px-8"
                 :class="{
                   'dvs-cursor-pointer': !file.on,
-                  'dvs-border-b dvs-border-lighter': listMode,
-                  'dvs-p-4 dvs-px-8': listMode,
-                  'dvs-p-0 dvs-m-2' : !listMode
+                  'dvs-border-b dvs-border-lighter': listMode
                 }"
                 @click="openFile(file)">
 
                 <!-- Close File if On -->
-                <i class="ion-android-close dvs-absolute dvs-pin-t dvs-pin-r dvs-mt-4 dvs-mr-4 dvs-cursor-pointer" v-if="file.on" @click.stop.prevent="closeFile(file)"></i>
-
+                <div v-if="file.on" @click.stop.prevent="closeFile(file)">
+                  <close-icon class="dvs-absolute dvs-pin-t dvs-pin-r dvs-mt-4 dvs-mr-4 dvs-cursor-pointer" :w="30" :h="30" />
+                </div>
                 <!-- Closed File -->
                 <div v-if="!file.on">
 
                   <!-- List Mode -->
                   <div class="dvs-flex dvs-justify-between dvs-items-center" v-if="listMode">
                     <img :src="file.thumb" height="50">
-                    <h5>{{ file.name }}</h5>
+                    <h6 class="dvs-text-sm">{{ file.name }}</h6>
                     <div class="dvs-rounded-full" :class="{
                       'dvs-bg-green-dark': isActive(file),
                       'dvs-bg-grey': !isActive(file)
@@ -102,73 +110,48 @@
                   </div>
 
                   <!-- Grid Mode -->
-                  <div class="dvs-grid-preview" :style="`background-image:url(${file.url})`" v-else>
-                    <h6 class="dvs-text-xs dvs-w-full dvs-py-2 dvs-bg-black-50 dvs-text-white dvs-absolute dvs-pin-b dvs-text-center dvs-rounded-sm">{{ file.name }}</h6>
+                  <div class="dvs-grid-preview" :style="`background-size:cover;background-image:url('${file.url}')`" v-else>
+                    <h6 class="dvs-text-xs dvs-w-full dvs-py-2 dvs-bg-black-50 dvs-text-white dvs-absolute dvs-pin-b dvs-pin-l dvs-pin-r dvs-text-center dvs-rounded-sm">{{ file.name }}</h6>
                   </div>
                 </div>
 
                 <!-- Open File -->
                 <div v-else class="dvs-flex dvs-p-4">
-                  <!-- List Mode -->
-                  <template v-if="listMode">
-                    <div class="dvs-w-1/2 dvs-mr-8 dvs-flex dvs-flex-col dvs-justify-between">
-                      <img :src="file.url" class="dvs-cursor-pointer" @click="selectFile(file)">
-                      <div class="dvs-flex">
-                        <i class="ion-trash-a dvs-text-2xl dvs-mr-4 dvs-cursor-pointer" v-devise-alert-confirm="{callback: requestDeleteFile, arguments: file, message: 'Are you sure you want to delete this media?'}"></i>
-                        <a :href="file.url" target="_blank"><i class="ion-link dvs-mt-4 dvs-mr-4 dvs-cursor-pointer dvs-text-2xl"></i></a>
+                  <div class="dvs-w-1/2 dvs-mr-8 dvs-flex dvs-flex-col dvs-justify-between">
+                    <img :src="file.url" class="dvs-cursor-pointer dvs-mb-4" @click="selectFile(file)">
+                    <div class="dvs-flex">
+                      <div class="dvs-mr-4 dvs-cursor-pointer" v-devise-alert-confirm="{callback: requestDeleteFile, arguments: file, message: 'Are you sure you want to delete this media?'}">
+                        <trash-icon :h="30" :w="30" :style="{color: theme.adminBackground.color}" />
                       </div>
+                      <a :href="file.url" target="_blank" :style="{color: theme.adminBackground.color}">
+                        <link-icon :h="30" :w="30" />
+                      </a>
                     </div>
-                    <div class="dvs-w-1/2">
-                      <h6>Filename</h6>
-                      <p>{{ file.name }}</p>
+                  </div>
+                  <div class="dvs-w-1/2">
+                    <h6 class="dvs-text-sm">Filename</h6>
+                    <p class="dvs-text-base">{{ file.name }}</p>
 
-                      <h6 class="dvs-mt-4">Size</h6>
-                      <p>{{ file.size }}</p>
+                    <fieldset class="dvs-fieldset">
+                      <h6 class="dvs-text-sm">URL</h6>
+                      <input type="text" :value="file.url">
+                    </fieldset>
 
-                      <p><button @click="selectFile(file)" class="dvs-btn">Select</button></p>
+                    <h6 class="dvs-mt-4 dvs-text-sm">Size</h6>
+                    <p>{{ file.size }}</p>
 
-                      <template v-if="isActive(file)">
-                        <h6 class="dvs-my-2">Appears On</h6>
-                        <ul class="dvs-list-reset">
-                          <li v-for="field in file.fields" class="dvs-py-2">
-                            <a :href="field.page_slug" target="_blank" class="dvs-btn dvs-btn-sm">{{ field.page_title }} - {{ field.field_name }}</a>
-                          </li>
-                        </ul>
-                      </template>
+                    <p><button @click="selectFile(file)" class="dvs-btn" :style="actionButtonTheme">Select</button></p>
 
-                    </div>
-                  </template>
-                  <!-- Grid Mode -->
-                  <template v-else>
-                    <div class="dvs-w-1/2 dvs-mr-8 dvs-flex dvs-flex-col dvs-justify-between">
-                      <a :href="file.url" target="_blank"><img :src="file.url"></a>
-                      <i class="ion-trash-a dvs-mt-4 dvs-mr-4 dvs-cursor-pointer dvs-text-xl" v-devise-alert-confirm="{callback: requestDeleteFile, arguments: file, message: 'Are you sure you want to delete this media?'}"></i>
-                    </div>
-                    <div class="dvs-w-1/2">
-                      <h6>Filename</h6>
-                      <p>{{ file.name }}</p>
+                    <template v-if="isActive(file)">
+                      <h6 class="dvs-my-2 dvs-text-sm">Appears On</h6>
+                      <ul class="dvs-list-reset">
+                        <li v-for="field in file.fields" :key="field.id" class="dvs-py-2">
+                          <a :href="field.page_slug" target="_blank" class="dvs-btn dvs-btn-sm">{{ field.page_title }} - {{ field.field_name }}</a>
+                        </li>
+                      </ul>
+                    </template>
 
-                      <fieldset class="dvs-fieldset">
-                        <label>URL</label>
-                        <input type="text" :value="file.url">
-                      </fieldset>
-
-                      <h6 class="dvs-mt-4">Size</h6>
-                      <p>{{ file.size }}</p>
-
-                      <p><button @click="selectFile(file)" class="dvs-btn">Select</button></p>
-
-                      <template v-if="isActive(file)">
-                        <h6 class="dvs-my-2">Appears On</h6>
-                        <ul class="dvs-list-reset">
-                          <li v-for="field in file.fields" class="dvs-py-2">
-                            <a :href="field.page_slug" target="_blank" class="dvs-btn dvs-btn-sm">{{ field.page_title }} - {{ field.field_name }}</a>
-                          </li>
-                        </ul>
-                      </template>
-
-                    </div>
-                  </template>
+                  </div>
                 </div>
               </li>
             </ul>
@@ -185,6 +168,12 @@
   import Loadbar from './../utilities/Loadbar'
   import Breadcrumbs from './Breadcrumbs'
   import vue2Dropzone from 'vue2-dropzone'
+
+  import FolderIcon from 'vue-ionicons/dist/ios-folder.vue'
+  import TrashIcon from 'vue-ionicons/dist/md-trash.vue'
+  import CloseIcon from 'vue-ionicons/dist/ios-close.vue'
+  import AttachIcon from 'vue-ionicons/dist/md-attach.vue'
+  import LinkIcon from 'vue-ionicons/dist/ios-link.vue'
 
   export default {
     data () {
@@ -308,7 +297,7 @@
       dropzoneOptions () {
         return {
           url: '/api/devise/media?directory=' + this.currentDirectory,
-          dictDefaultMessage: "<span class='dvs-cursor-pointer'><i class='ion-android-attach'></i> Upload</span>",
+          dictDefaultMessage: "<span class='dvs-cursor-pointer'>Upload New File</span>",
           method: 'post',
           createImageThumbnails: false,
           headers: {
@@ -320,7 +309,12 @@
     components: {
       'loadbar': Loadbar,
       'breadcrumbs': Breadcrumbs,
-      vueDropzone: vue2Dropzone
+      vueDropzone: vue2Dropzone,
+      AttachIcon,
+      FolderIcon,
+      LinkIcon,
+      TrashIcon,
+      CloseIcon
     }
   }
 </script>

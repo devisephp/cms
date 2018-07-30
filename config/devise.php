40ae43f182a53721df0ae6880400f7def99a2e75
Mauro Velez <mauro@lbm.co>
2018-07-30 12:59:13 -0400

    basic permissions rc

--- config/devise.php
+++ config/devise.php
@@ -2,30 +2,71 @@
 
 return [
 
-  /*
-  |--------------------------------------------------------------------------
-  | Media
-  |--------------------------------------------------------------------------
-  |
-  | Configuration for the media manager
-  | See config/filesystems.php for media.driver options
-  |
-  */
+    /*
+    |--------------------------------------------------------------------------
+    | Media
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for the media manager
+    | See config/filesystems.php for media.driver options
+    |
+    */
 
-  'media' => [
-    'disk' => env('DVS_FILESYSTEM_DISK', 'public'),
-    'root-directory' => 'media',
-  ],
+    'media' => [
+        'disk' => env('DVS_FILESYSTEM_DISK', 'public'),
+        'root-directory' => 'media',
+    ],
 
-  /*
-  |--------------------------------------------------------------------------
-  | Mothership
-  |--------------------------------------------------------------------------
-  |
-  | Configuration for mothership
-  |
-  */
-  'mothership' => [
-    'api-key' => env('MOTHERSHIP_API_KEY', null)
-  ]
+    /*
+    |--------------------------------------------------------------------------
+    | Mothership
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for mothership
+    |
+    */
+    'mothership' => [
+        'api-key' => env('MOTHERSHIP_API_KEY', null)
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Permissions
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for user permissions.
+    | Available Permissions: ['access admin','manage pages','manage users','manage mothership','manage templates',
+    | 'manage settings','manage meta','manage sites','manage languages','manage redirects']
+    |
+    */
+    [
+        'permissions' => [
+            /*
+            'example@email.com' => [
+                'access admin',
+                'manage pages',
+                'manage users',
+                'manage mothership',
+                'manage templates',
+                'manage settings',
+                'manage meta',
+                'manage sites',
+                'manage languages',
+                'manage redirects'
+            ],
+            */
+            'default' => [
+                'access admin',
+                'manage pages',
+                'manage users',
+                'manage mothership',
+                'manage templates',
+                'manage settings',
+                'manage meta',
+                'manage sites',
+                'manage languages',
+                'manage redirects'
+            ]
+        ]
+    ]
 ];

52d7370516652aea584bc4ec78e0da39a3d27dd9
Mauro Velez <mauro@lbm.co>
2018-08-02 18:24:22 -0400

    updates to language data

--- Devise/Http/Resources/Vue/LanguageResource.php
+++ Devise/Http/Resources/Vue/LanguageResource.php
@@ -2,23 +2,30 @@
 
 namespace Devise\Http\Resources\Vue;
 
+use Devise\Sites\SiteDetector;
 use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Support\Facades\App;
 
 class LanguageResource extends Resource
 {
-  /**
-   * Transform the resource into an array.
-   *
-   * @param  \Illuminate\Http\Request
-   * @return array
-   */
-  public function toArray($request)
-  {
-    return [
-      'id'   => $this->id,
-      'name' => $this->name,
-      'default' => $this->pivot->default,
-      'path' => ($this->pivot->default) ? '/' : '/' . $this->code,
-    ];
-  }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $detector = App::make(SiteDetector::class);
+        $site = $detector->current();
+
+        return [
+            'id'      => $this->id,
+            'name'    => $this->name,
+            'default' => $this->pivot->default,
+            'path'    => ($this->pivot->default) ? '/' : '/' . $this->code,
+            'url'     => ($this->pivot->default) ? $site->url . '/' : $site->url . '/' . $this->code,
+        ];
+    }
 }

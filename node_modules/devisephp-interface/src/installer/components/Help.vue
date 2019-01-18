6a25725dcfd3da0a665405a810301130c6af3bd5
Gary Williams <gary@lbm.co>
2019-01-18 15:23:47 -0500

    whoops no node modules needed

--- node_modules/devisephp-interface/src/installer/components/Help.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div
-    class="dvs-mb-8 dvs-relative dvs-bg-green-lightest dvs-p-6 dvs-pl-16 dvs-relative dvs-border dvs-rounded dvs-border-green dvs-text-green"
-  >
-    <alert-icon w="40" h="40" class="dvs-mr-4 dvs-absolute dvs-pin-l dvs-pin-t dvs-mt-6 dvs-ml-4"/>
-    <slot></slot>
-  </div>
-</template>
--- node_modules/devisephp-interface/src/sass/postcss.config.js
+++
@@ -1,7 +0,0 @@
-var tailwindcss = require('tailwindcss');
-module.exports = {
-  plugins: [
-    tailwindcss('./vendor/devisephp/cms/vue/src/tailwind/tailwind.js'),
-    require('autoprefixer')
-  ]
-}

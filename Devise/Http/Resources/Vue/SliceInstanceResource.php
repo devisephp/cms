dbf0bd19c00ee9dcbc5815655198e8fd6d415325
Mauro Velez <mauro@lbm.co>
2019-02-27 16:06:35 -0500

    fixed records check

--- Devise/Http/Resources/Vue/SliceInstanceResource.php
+++ Devise/Http/Resources/Vue/SliceInstanceResource.php
@@ -6,6 +6,7 @@ use Devise\Models\Repository as ModelRepository;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\App;
 
 class SliceInstanceResource extends Resource
@@ -57,7 +58,7 @@ class SliceInstanceResource extends Resource
         $all = [];
         if ($records)
         {
-            if ($records instanceof Collection)
+            if ($records instanceof Collection || $records instanceof LengthAwarePaginator)
             {
                 foreach ($records as $record)
                 {

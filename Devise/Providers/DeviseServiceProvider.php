2293bb79698dc2df9706c1f5b1b3732b2c3248dd
Mauro Velez <mauro@lbm.co>
2019-04-11 08:02:03 -0400

    updated assets command to publish interface dir

--- Devise/Providers/DeviseServiceProvider.php
+++ Devise/Providers/DeviseServiceProvider.php
@@ -93,7 +93,7 @@ class DeviseServiceProvider extends ServiceProvider
     private function setPublishables()
     {
         $this->publishes([
-            __DIR__ . '/../../build' => public_path('devise'),
+            __DIR__ . '/../../interface' => public_path('devise'),
         ], 'dvs-assets');
 
         $this->publishes([

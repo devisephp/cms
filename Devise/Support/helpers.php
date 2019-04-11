8e9ec515ef84f9117a7c1cd4991baea6087493ba
Mauro Velez <mauro@lbm.co>
2019-04-11 08:19:24 -0400

    added helpers file and autoloading with composer

---
+++ Devise/Support/helpers.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Support\HtmlString;
+
+if (!function_exists('vuemix'))
+{
+    /**
+     * Get the path to a versioned Mix file.
+     *
+     * @param  string $path
+     * @param  string $manifestDirectory
+     * @return \Illuminate\Support\HtmlString|string
+     *
+     * @throws \Exception
+     */
+    function vuemix($path, $manifestDirectory = '')
+    {
+        $hotFilePath = public_path($manifestDirectory . '/hot');
+        $pathParts = explode(DIRECTORY_SEPARATOR, $path);
+        $file = $pathParts[count($pathParts) - 1];
+
+        // check if HMR server is running via helper file 'hot'
+        if (file_exists($hotFilePath))
+        {
+            if (strpos($file, 'chunk-vendor') !== false)
+            {
+                return new HtmlString('');
+            }
+            // Everything is bundled in the app.js so only include it
+            $hotFile = file($hotFilePath);
+            $resourcePath = $file === 'app.js' ? $hotFile[0] . 'app/' . $file : $hotFile[0] . 'app/' . $path;
+
+            return new HtmlString($resourcePath);
+        }
+
+        // HMR isn't loading so check the manifest and return the hashed file
+        $manifestFilePath = public_path($manifestDirectory . '/manifest.json');
+        $manifestFile = file_get_contents($manifestFilePath);
+        $manifest = json_decode($manifestFile);
+
+        return new HtmlString($manifestDirectory . DIRECTORY_SEPARATOR . $manifest->$file); // return path without changing anything aka production
+    }
+}

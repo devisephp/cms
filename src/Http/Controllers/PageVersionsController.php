28b88e793f79eac7f1df5abadc64e5309d4b1639
Mauro <mauro@lbm.co>
2022-02-09 11:47:15 -0500

    updated version to include live language and ignore soft deleted records

--- src/Http/Controllers/PageVersionsController.php
+++ src/Http/Controllers/PageVersionsController.php
@@ -33,32 +33,42 @@ class PageVersionsController extends Controller
 
     public function index(ApiRequest $request)
     {
-        $pages = $this->DvsPageVersion
+        $pages = DvsPageVersion::with('page.currentVersion')
             ->join('dvs_pages', 'dvs_pages.id', '=', 'dvs_page_versions.page_id')
             ->join('dvs_languages', 'dvs_languages.id', '=', 'dvs_pages.language_id')
-            ->join('dvs_sites', 'dvs_sites.id', '=', 'dvs_pages.site_id');
+            ->join('dvs_sites', 'dvs_sites.id', '=', 'dvs_pages.site_id')
+            ->whereNull('dvs_page_versions.deleted_at')
+            ->whereNull('dvs_pages.deleted_at');
 
         $pages = $pages->where('dvs_pages.title', 'LIKE', '%' . $request->get('term') . '%');
-        
+
         if ($request->has('site_id')) {
             $pages = $pages->where('dvs_pages.site_id', '=', $request->get('site_id'));
         }
 
-        $pages = $pages->select('dvs_page_versions.id', 'dvs_pages.title', 'dvs_pages.site_id', 'dvs_sites.name as site_name', 'dvs_languages.code', 'dvs_page_versions.name')
+        $pages = $pages->select(
+            'dvs_page_versions.id',
+            'dvs_page_versions.page_id',
+            'dvs_pages.title',
+            'dvs_pages.site_id',
+            'dvs_sites.name as site_name',
+            'dvs_languages.code',
+            'dvs_page_versions.name'
+        )
             ->orderBy('dvs_pages.site_id')
             ->take(10)
             ->get();
 
         $results = [];
-        foreach ($pages as $page)
-        {
+        foreach ($pages as $page) {
             $results[$page->id] = [
-                'id'           => $page->id,
-                'site'         => $page->site_name,
-                'site_id'      => $page->site_id,
-                'title'        => $page->title,
+                'id' => $page->id,
+                'site' => $page->site_name,
+                'site_id' => $page->site_id,
+                'title' => $page->title . ' [' . $page->name . '] ',
                 'version_name' => $page->name,
-                'language'     => $page->code
+                'is_live' => ($page->page->currentVersion) ? ($page->page->currentVersion->id == $page->id) : false,
+                'language' => $page->code
             ];
         }
 
@@ -73,7 +83,10 @@ class PageVersionsController extends Controller
      */
     public function copy(StorePageVersion $request)
     {
-        $version = $this->PageVersionManager->copyPageVersion($request->input('page_version_id'), $request->input('name'));
+        $version = $this->PageVersionManager->copyPageVersion(
+            $request->input('page_version_id'),
+            $request->input('name')
+        );
 
         return new PageVersionResource($version);
     }
@@ -82,7 +95,7 @@ class PageVersionsController extends Controller
      * Request a page version be updated
      *
      * @param UpdatePageVersion $request
-     * @param  integer $pageVersionId
+     * @param integer $pageVersionId
      * @return PageVersionResource
      * @internal param array $input
      */
@@ -97,7 +110,7 @@ class PageVersionsController extends Controller
      * Request that the page version sharing be toggled
      *
      * @param ApiRequest $request
-     * @param  integer $pageVersionId
+     * @param integer $pageVersionId
      * @return PageVersionResource
      */
     public function toggleSharing(ApiRequest $request, $pageVersionId)
@@ -111,7 +124,7 @@ class PageVersionsController extends Controller
      * Request page version be destroyed
      *
      * @param ApiRequest $request
-     * @param  integer $pageVersionId
+     * @param integer $pageVersionId
      */
     public function delete(ApiRequest $request, $pageVersionId)
     {
